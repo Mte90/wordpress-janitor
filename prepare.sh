1a3812771d89ef53e007692d40135be7455fe02e
Daniele Scasciafratte <mte90net@gmail.com>
2018-03-09 16:05:29 +0100

    updates

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,7 @@
 cd /home/user/wordpress/src/
 # Create WordPress config.
 if ! [ -f ./wp-config.php ]; then
+    echo "WordPress Installing in progress"
     mysql -u root --password=wp -e "CREATE DATABASE IF NOT EXISTS wordpress_develop" 2>/dev/null
     mysql -u root --password=wp -e "GRANT ALL PRIVILEGES ON wordpress_develop.* TO wp@localhost IDENTIFIED BY 'wp';" 2>/dev/null
     mysql -u root --password=wp -e "CREATE DATABASE IF NOT EXISTS wordpress_unit_tests" 2>/dev/null
@@ -17,4 +18,5 @@ PHP
     mkdir -p ./wp-content/uploads
     sudo chown -R www-data ./wp-content
     sudo chmod -R 775 ./wp-content
+    echo "WordPress Installed"
 fi
